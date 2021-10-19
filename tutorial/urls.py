ec4a301c970fa1073bb3186bdac0bd1980aa6118
Eli Storlie <20595774+eli215@users.noreply.github.com>
2021-10-18 21:29:45 -0700

    Pagination and Settings

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from rest_framework import routers
 from tutorial.quickstart import views
 
-router = routers.DefaultRouter
+router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
